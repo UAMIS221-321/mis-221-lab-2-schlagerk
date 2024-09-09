f012dbee3265392e2309ac4089f9b688e163369f
Kate Schlager <kcschlager@crimson.ua.edu>
2024-09-08 22:27:40 -0500

    Initial Commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+const double COST_OF_SANDWICH = 4.75; 
+const double COST_OF_TOPPING = 0.55;
+const double DISCOUNT_AMOUNT = 0.1;
+
+//sandwiches
+int numberOfSandwiches = GetNumberOfSandwiches();
+static int GetNumberOfSandwiches()
+{
+    Console.WriteLine("How many sandwiches?");
+    return int.Parse(Console.ReadLine());
+}
+
+//toppings 
+int numberOfToppings = GetNumberOfToppings();
+static int GetNumberOfToppings()
+{
+    Console.WriteLine("How many toppings?");
+    return int.Parse(Console.ReadLine());
+}
+
+//tip
+double tip = GetTip();
+static double GetTip()
+{
+    Console.WriteLine("How much tip?");
+    return double.Parse(Console.ReadLine());
+}
+
+double totalSandwichCost = GetTotalSandwichCost(numberOfSandwiches, COST_OF_SANDWICH);
+static double GetTotalSandwichCost(int numberOfSandwiches, double COST_OF_SANDWICH)
+{
+    return numberOfSandwiches * COST_OF_SANDWICH;
+}
+
+double totalToppingCost = GetTotalToppingCost(numberOfToppings, COST_OF_TOPPING);
+static double GetTotalToppingCost(int numberOfToppings, double COST_OF_TOPPING)
+{
+    return numberOfToppings * COST_OF_TOPPING;
+}
+
+double baseCost = GetBaseCost(totalSandwichCost, totalToppingCost);
+static double GetBaseCost(double totalSandwichCost, double totalToppingCost)
+{
+    return totalSandwichCost + totalToppingCost;
+}
+
+double orderCost = tip + baseCost * (1 - DISCOUNT_AMOUNT);
+
+    System.Console.WriteLine("The order cost is "+orderCost);
--- obj/Debug/net8.0/mis-221-lab-2-schlagerk.AssemblyInfo.cs
+++ obj/Debug/net8.0/mis-221-lab-2-schlagerk.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mis-221-lab-2-schlagerk")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6beb5fcdfcf434119a0b4b5205353d107cac576")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7ab7769649fd019e52500367fdfeec2979fffa5")]
 [assembly: System.Reflection.AssemblyProductAttribute("mis-221-lab-2-schlagerk")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mis-221-lab-2-schlagerk")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
